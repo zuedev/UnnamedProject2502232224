9881ff1cf9f217c1ac3613abfebc389a27253d5e
Alex Pooley <zuedev@gmail.com>
2025-02-23 23:29:36 +0000

    add stars and have them follow player movement

---
+++ Assets/UnnamedProject2502232224/scene_UdonProgramSources/StarsFollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+/*
+ * StarsFollowPlayer.cs
+ *
+ * This script is attached to the stars in the sky. It makes the stars follow the player.
+ */
+public class StarsFollowPlayer : UdonSharpBehaviour
+{
+    void Start()
+    {
+        // Set the initial position of the stars to the player's position
+        transform.position = Networking.LocalPlayer.GetPosition();
+    }
+
+    void Update()
+    {
+        // Set the position of the stars to the player's position
+        transform.position = Networking.LocalPlayer.GetPosition();
+    }
+}
